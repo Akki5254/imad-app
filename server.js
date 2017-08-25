d8c05c7d471ae59f29dc73355a67b4af3f5331b3
Akshay Meshram <30983975+Akki5254@users.noreply.github.com>
2017-08-25 22:16:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,17 +6,37 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = { 
-    title: 'Article One | Akshay Meshram',
-    heading: 'Article One',
-    date: '25 August 2017',
-    content: `<p>
-                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-                </p>
-                
-                <p>
-                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-                </p>`
+var articles = {
+    'article-one': { 
+        title: 'Article One | Akshay Meshram',
+        heading: 'Article One',
+        date: '25 August 2017',
+        content: `<p>
+                        This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+                    </p>
+                    
+                    <p>
+                        This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+                    </p>`
+                    
+    },
+    'article-two': {
+        title: 'Article Two | Akshay Meshram',
+        heading: 'Article Two',
+        date: '26 August 2017',
+        content: `<p>
+                        This is the content of my second article.
+                    </p>`
+                    
+    },
+    'article-three': {
+        title: 'Article Three | Akshay Meshram',
+        heading: 'Article Three',
+        date: '27 August 2017',
+        content: `<p>
+                        This is the content of my third article.
+                    </p>`
+    }
     
 };
 
@@ -64,16 +84,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one' , function(req, res){
-    res.send(createTemplate(articleOne))
-});
-
-app.get('/article-two' , function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three' , function(req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName' , function(req, res){
+    
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]))
 });
 
 
