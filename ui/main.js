6db25c7822e3b3be551da8f7ac66f22e25f65847
Akshay Meshram <30983975+Akki5254@users.noreply.github.com>
2017-09-01 14:22:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,16 +11,25 @@ button.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+             //Capture a list of names and render it as a list
+             var names = request.responseText;
+             names = JSON.parse(names);
+                var names = ['name1', 'name2', 'name3', 'name4'];
+                var list = '';
+                for(var i=0; i< names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                    
+                }
+                
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;   
             }
         }
         //Not Done Yet
     }; 
     
     //Make the Request
-    request.open('GET', 'http://itsmetheviper26.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://itsmetheviper26.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
 
