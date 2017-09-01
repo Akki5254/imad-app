a468ee863869e3bdaee8cdfed02e360799811598
Akshay Meshram <30983975+Akki5254@users.noreply.github.com>
2017-09-01 12:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-// change the text of main-text div
+//change the text of main-text div
 var elemnt = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
