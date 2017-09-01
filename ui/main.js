0e2474721fa91e8729817c2ac2e0748af7208a6c
Akshay Meshram <30983975+Akki5254@users.noreply.github.com>
2017-09-01 14:29:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,8 @@ button.onclick = function(){
             if(request.status === 200){
              //Capture a list of names and render it as a list
              var names = request.responseText;
-             names = JSON.parse(names);
+             names = JSON.parse(names)
+                
                 var list = '';
                 for(var i=0; i< names.length; i++){
                     list += '<li>' + names[i] + '</li>';
@@ -34,11 +35,13 @@ button.onclick = function(){
 
 //Submit name
 
+
+
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make a request to server and send the names
     
-  
+    
 };
