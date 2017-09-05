65595bd9f7d7a5a943bebca9513f5a27f62a2938
Akshay Meshram <30983975+Akki5254@users.noreply.github.com>
2017-09-05 23:12:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,12 +132,11 @@ app.get('/articles/:articleName' , function(req, res){
                 res.status(404).send('Article Not Found');
             }else{
                 var articleData = result.rows[0];
-                res.send(createTemplate(articles[articleData]));
+                res.send(createTemplate(articleData));
             }
         }
     });
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
