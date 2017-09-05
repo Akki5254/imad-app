ae54804d0e637e8de9b02d78cd5116a0be6313c7
Akshay Meshram <30983975+Akki5254@users.noreply.github.com>
2017-09-05 23:32:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,41 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-
-var articles = {
-    'article-one': { 
-        title: 'Article One | Akshay Meshram',
-        heading: 'Article One',
-        date: '25 August 2017',
-        content: `<p>
-                        This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-                    </p>
-                    
-                    <p>
-                        This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-                    </p>`
-                    
-    },
-    'article-two': {
-        title: 'Article Two | Akshay Meshram',
-        heading: 'Article Two',
-        date: '26 August 2017',
-        content: `<p>
-                        This is the content of my second article.
-                    </p>`
-                    
-    },
-    'article-three': {
-        title: 'Article Three | Akshay Meshram',
-        heading: 'Article Three',
-        date: '27 August 2017',
-        content: `<p>
-                        This is the content of my third article.
-                    </p>`
-    }
-    
-};
-
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
